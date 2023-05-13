3d802684a49dc9149c59c3cf35f2aec9a812f062
Brianne Siemsen <briannesiemsen@gmail.com>
2023-05-12 18:23:48 -0700

    worked on project and credits pages and almost finished home and resume
    put project images in, rearranged resume, finished home animations, and created credits page

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 $(window).on("load", function(){
-    let $margin = $(".homeProjects").css("marginLeft");
-    //$(".homeProjects").css("marginLeft", "100vw");
+    $(".homeProjects").css("marginLeft", "100vw");
     $(".homePageLight").slideDown("slow", function(){
-        $(this).find("*").fadeTo("slow", 1/*, function(){
+        $(this).find("*").fadeTo("slow", 1, function(){
             $(".homeProjects").animate({
-                padding: {
-                    left: `-=100vw`
-                } 
-            })
-        }*/);
+                marginLeft: 0
+            }, "slow")
+        });
     });
 })
